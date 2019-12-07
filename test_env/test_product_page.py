80c5b45ffd0d5e36a9f25dbc70a36beb47912c12
skywalker182 <nast.shiyanova@gmail.com>
2019-12-08 02:17:23 +0300

    Update test_product_page.py

--- test_env/test_product_page.py
+++ test_env/test_product_page.py
@@ -6,7 +6,7 @@ from .pages.test_data import ProductTestData, LoginTestData
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-#@pytest.mark.need_review
+@pytest.mark.need_review
 def test_user_can_add_product_to_basket(browser):
         page = ProductPage(browser, link)
         main_page = MainPage(browser, link)
@@ -18,7 +18,7 @@ def test_user_can_add_product_to_basket(browser):
         page.add_product_to_basket()
         page.should_be_successful_message()
 
-#@pytest.mark.need_review
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
         page = ProductPage(browser, link)
         page.open()
@@ -26,7 +26,7 @@ def test_guest_can_add_product_to_basket(browser):
         page.add_product_to_basket()
         page.should_be_successful_message()
 
-#@pytest.mark.need_review
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
         page = ProductPage(browser, link)
         page.open()
@@ -35,7 +35,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
         page.open_basket()
         page.should_be_expected_name(expected_name)
 
-#@pytest.mark.need_review
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
         page = ProductPage(browser, link)
         page.open()
@@ -44,8 +44,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
         main_page = MainPage(browser, link)
         main_page.should_be_login_link()
 
-@pytest.mark.need_review
-def test_user_can_add_to_fav(browser):
+@pytest.mark.need_review_custom_scenarios
+def test_user_should_see_fav_button(browser):
         page = ProductPage(browser, link)
         main_page = MainPage(browser, link)
         page.open()
@@ -54,5 +54,4 @@ def test_user_can_add_to_fav(browser):
         login_page.login(LoginTestData.email, LoginTestData.password)
         page.search(ProductTestData.expected_name)
         page.open_product()
-        page.add_product_to_fav()
-        page.should_be_successful_message()
+        page.should_be_fav_button()
