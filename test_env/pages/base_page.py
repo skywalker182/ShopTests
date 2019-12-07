cfbdb3f2bc302f9e2f3e47b403ee12a281eb393b
skywalker182 <nast.shiyanova@gmail.com>
2019-12-08 02:40:09 +0300

    Update base_page.py

--- test_env/pages/base_page.py
+++ test_env/pages/base_page.py
@@ -1,4 +1,4 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 class BasePage():
     def __init__(self, browser, url):
