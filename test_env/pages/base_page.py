f19ef0b614ce612fc0d39188a9d1038f14e29e8f
skywalker182 <nast.shiyanova@gmail.com>
2019-12-08 02:35:34 +0300

    Update base_page.py

--- test_env/pages/base_page.py
+++ test_env/pages/base_page.py
@@ -19,4 +19,10 @@ class BasePage():
         except (NoSuchElementException):
             return False
         return True
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+           WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
 
+        return False
